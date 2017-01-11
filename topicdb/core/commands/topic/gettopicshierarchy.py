09a3b0df7262677aac41e49371b71bea06fc6c68
Brett Kromkamp <brett.kromkamp@gmail.com>
2017-01-11 18:33:03 +0100

    Removed hard-code maximum-depth variable (replaced with a parameter).

--- topicdb/core/commands/topic/gettopicshierarchy.py
+++ topicdb/core/commands/topic/gettopicshierarchy.py
@@ -17,24 +17,25 @@ class GetTopicsHierarchy:
 
     def __init__(self, database_path, topic_map_identifier,
                  identifier='',
-                 cumulative_distance=0,
+                 maximum_depth=10,
+                 cumulative_depth=0,
                  accumulative_tree=None,
                  accumulative_nodes=None):
         self.database_path = database_path
         self.topic_map_identifier = topic_map_identifier
         self.identifier = identifier
-        self.cumulative_distance = cumulative_distance
+        self.maximum_depth = maximum_depth
+        self.cumulative_depth = cumulative_depth
         self.accumulative_tree = accumulative_tree
         self.accumulative_nodes = accumulative_nodes
-
-        self.maximum_depth = 10
 
     def execute(self):
         if self.identifier == '':
             raise TopicStoreError("Missing 'identifier' parameter")
         if self.accumulative_tree is None:
             tree = Tree()
-            root_topic = GetTopic(self.database_path, self.topic_map_identifier, self.identifier).execute()
+            root_topic = GetTopic(self.database_path, self.topic_map_identifier,
+                                  self.identifier).execute()
             tree.add_node(self.identifier, parent=None, topic=root_topic)
         else:
             tree = self.accumulative_tree
@@ -44,7 +45,7 @@ class GetTopicsHierarchy:
         else:
             nodes = self.accumulative_nodes
 
-        if self.cumulative_distance <= self.maximum_depth:  # Exit case.
+        if self.cumulative_depth <= self.maximum_depth:  # Exit case.
             associations = GetTopicAssociations(
                 self.database_path, self.topic_map_identifier, self.identifier).execute()
             for association in associations:
@@ -59,7 +60,7 @@ class GetTopicsHierarchy:
                         nodes.append(topic_ref)
             children = tree[self.identifier].children
             for child in children:
-                self.cumulative_distance += 1
+                self.cumulative_depth += 1
                 self.identifier = child
                 self.accumulative_tree = tree
                 self.accumulative_nodes = nodes
