c0ce6310c63851d94b4ee08bfb0c8516ee008817
Brett Kromkamp <brett.kromkamp@gmail.com>
2017-02-24 21:01:38 +0100

    Added TopicField enum.

---
+++ topicdb/core/store/topicfield.py
@@ -0,0 +1,13 @@
+"""
+TopicField enumeration. Part of the StoryTechnologies project.
+
+July 16, 2016
+Brett Alistair Kromkamp (brett.kromkamp@gmail.com)
+"""
+
+from enum import Enum
+
+
+class TopicField(Enum):
+    IDENTIFIER = 0
+    BASE_NAME = 1
