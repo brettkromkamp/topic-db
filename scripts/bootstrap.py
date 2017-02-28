36b5b9b3fa6f135b76ee2a6bf06010724f14e0dd
Brett Kromkamp <brett.kromkamp@gmail.com>
2017-02-28 18:51:56 +0100

    Minor change to bootstrap.py file.

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -9,7 +9,6 @@ from topicdb.core.store.topicstore import TopicStore
 
 store = TopicStore("localhost", "storytech", "5t0ryt3ch!")
 store.open()
-store.set_topic_map(1, "The Doomsday Plans",
-                    "A soldier has to steal the plans for a secret weapon.")
-store.set_topic_map(2, "An Unexpected Meeting", "Two people meet ever so briefly.")
+store.set_topic_map(1, "The Doomsday Plans","A soldier has to steal the plans for a secret weapon.", entry_topic="outpost")
+store.set_topic_map(2, "An Unexpected Meeting", "Two people meet ever so briefly.", entry_topic="cafeteria")
 store.close()
