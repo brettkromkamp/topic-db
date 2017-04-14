37c52542c8d69072a8669b765a608cb4a4d7b9b2
Brett Kromkamp <brettkromkamp@gmail.com>
2017-04-14 09:09:19 +0200

    Minor code formatting fix.

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -22,7 +22,7 @@ password = config['DATABASE']['Password']
 store = TopicStore(username, password)
 store.open()
 
-store.set_topic_map(1, "The Doomsday Plans","A soldier has to steal the plans for a secret weapon.", entry_topic="outpost")
+store.set_topic_map(1, "The Doomsday Plans", "A soldier has to steal the plans for a secret weapon.", entry_topic="outpost")
 store.set_topic_map(2, "An Unexpected Meeting", "Two people meet ever so briefly.", entry_topic="cafeteria")
 
 store.close()
