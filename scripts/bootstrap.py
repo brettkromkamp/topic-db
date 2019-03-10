d48134f697d1e9ec523c97ebb06d57e767ca4188
Brett Kromkamp <brettkromkamp@gmail.com>
2019-03-10 17:38:53 +0100

    Refactored topic map-specific functionality.

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -12,6 +12,8 @@ from topicdb.core.store.topicstore import TopicStore
 
 
 SETTINGS_FILE_PATH = os.path.join(os.path.dirname(__file__), '../settings.ini')
+USER_IDENTIFIER_1 = 1
+USER_IDENTIFIER_2 = 4
 
 config = configparser.ConfigParser()
 config.read(SETTINGS_FILE_PATH)
@@ -20,7 +22,16 @@ database_username = config['DATABASE']['Username']
 database_password = config['DATABASE']['Password']
 database_name = config['DATABASE']['Database']
 
-# Instantiate and open topic store.
+# Instantiate and open topic store, create and subsequently populate topic maps.
 with TopicStore(database_username, database_password, dbname=database_name) as store:
-    store.set_topic_map(1, 1, "Topic Map 1", "Topic Map 1 description.")
-    store.set_topic_map(1, 2, "Topic Map 2", "Topic Map 2 description.", public=True)
+    store.set_topic_map(USER_IDENTIFIER_1, "Topic Map 1", "Topic Map 1 description.")
+    store.set_topic_map(USER_IDENTIFIER_1, "Topic Map 2", "Topic Map 2 description.", public=True)
+    store.set_topic_map(USER_IDENTIFIER_2, "Topic Map 3", "Topic Map 3 description.")
+
+    # Populate topic maps (with pre-defined topics) for "USER_IDENTIFIER_1".
+    for topic_map in store.get_topic_maps(USER_IDENTIFIER_1):
+        store.populate_topic_map(topic_map.identifier)
+
+    # Populate topic maps (with pre-defined topics) for "USER_IDENTIFIER_2".
+    for topic_map in store.get_topic_maps(USER_IDENTIFIER_2):
+        store.populate_topic_map(topic_map.identifier)
