8455fc33fc67b10c7dbade908da00f73803b9491
Brett Kromkamp <brett.kromkamp@gmail.com>
2019-05-01 17:38:19 +0200

    Updated setup.py to reflect up-to-date Python versions.

--- setup.py
+++ setup.py
@@ -36,10 +36,10 @@ setup(name='topic-db',
           'License :: OSI Approved :: MIT License',
           'Natural Language :: English',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: Implementation :: CPython',
           'Topic :: Software Development :: Libraries :: Python Modules'
       ])
