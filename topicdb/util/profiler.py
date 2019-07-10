1815423f7fe06f456b8ea1a3d58f9e9939bb123d
Brett Kromkamp <brett.kromkamp@gmail.com>
2019-07-10 07:43:57 +0200

    Added profiling support.

---
+++ topicdb/util/profiler.py
@@ -0,0 +1,24 @@
+import time
+from functools import wraps
+from memory_profiler import memory_usage
+
+
+def profile(fn):
+    @wraps(fn)
+    def inner(*args, **kwargs):
+        fn_kwargs_str = ', '.join(f'{k}={v}' for k, v in kwargs.items())
+        print(f'\n{fn.__name__}({fn_kwargs_str})')
+
+        # Measure time
+        t = time.perf_counter()
+        return_value = fn(*args, **kwargs)
+        elapsed = time.perf_counter() - t
+        print(f'Time spent: {elapsed:0.4}')
+
+        # Measure memory
+        mem, return_value = memory_usage((fn, args, kwargs), retval=True, timeout=200, interval=1e-7)
+
+        print(f'Memory used: {max(mem) - min(mem)}')
+        return return_value
+
+    return inner
